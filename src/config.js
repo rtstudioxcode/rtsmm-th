a3c60e730f2042bf652513334024637a405b8a0d
RTStudioXCode <boymailody@gmail.com>
2025-10-28 08:08:38 +0700

    V25.1.13

--- src/config.js
+++ src/config.js
@@ -10,7 +10,7 @@ const envConfig = {
   port: Number(process.env.PORT || 3000),
   mongoUri: process.env.MONGO_URI, // ใช้สำหรับ Production
   // mongoUri: process.env.MONGO_URI || 'mongodb://admin:060843Za@147.50.240.76:27017/rtsmm-th?authSource=admin', // ใช้สำหรับทดสอบ
-  sessionSecret: process.env.SESSION_SECRET || 'rtstudioxcoe78rt58y9643y5t8y7u6i5o4p3',
+  sessionSecret: process.env.SESSION_SECRET || '',
   provider: {
     baseUrl: ((process.env.IPV_API_BASE || '').replace(/\/+$/, '')) || '',
     apiKey: process.env.IPV_API_KEY || ''
